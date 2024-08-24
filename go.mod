676c483f72921c092012ac4191d04030eb5e897c
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-08-25 01:06:54 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/QinYuuuu/SS23
 
 go 1.20
+
+require (
+	github.com/QinYuuuu/crypto v0.0.0-20240823133253-9c7c1153da42
+	github.com/stretchr/testify v1.9.0
+	github.com/vivint/infectious v0.0.0-20200605153912-25a574ae18a3
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- internal/smd/sender.go
+++ internal/smd/sender.go
@@ -1,6 +1,50 @@
 package smd
 
+import (
+	"github.com/QinYuuuu/SS23/crypto/hasher"
+	"github.com/QinYuuuu/SS23/reedsolomon"
+	"github.com/QinYuuuu/SS23/utils/polynomial"
+	"github.com/vivint/infectious"
+	"math/big"
+	"strconv"
+)
+
 type Sender struct {
+	n     int
+	t     int
+	p     *big.Int
+	coder reedsolomon.RScoder
 }
 
-func (s *Sender) Input(message []byte) {}
+func encrypt(k, m []byte) []byte{
+
+}
+
+func (s *Sender) Input(message [][]byte) {
+	if len(message) != s.n {
+		return
+	}
+	c := make([][]byte, s.n)
+	k := make([][][]byte, s.n)
+	for i := 0; i < s.n; i++ {
+		f, _ := polynomial.NewRand(s.t, s.p)
+		k[i] = make([][]byte, s.n)
+		for j := 0; j < s.n; j++ {
+			sij := f.EvalMod(new(big.Int).SetInt64(int64(j)), s.p)
+			k[i][j] = hasher.SHA256Hasher(append([]byte(strconv.Itoa(j)),sij.Bytes()...))
+		}
+		c[i] = k[i][0] ^ k[i][0]
+	}
+	chunk := make([][]infectious.Share, s.n)
+	for i := 0; i < s.n; i++ {
+		chunk[i] = s.coder.Encode(c[i])
+	}
+	merkleInput := make([][]byte, s.n)
+	for i := 0; i < s.n; i++ {
+		merkleInput[i] := make()
+		for j := 0; j < s.n; j++ {
+
+		}
+		 := f[i][]
+	}
+}
