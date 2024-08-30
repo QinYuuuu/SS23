cd7eeedd72a1f98bceb15a96518b50590ac1a66c
qbh <102593336+QinYuuuu@users.noreply.github.com>
2024-08-30 22:42:14 +0800

    Update receiver.go

--- internal/smd/receiver.go
+++ internal/smd/receiver.go
@@ -1,8 +1,13 @@
 package smd
 
-import "log"
+import (
+	"github.com/QinYuuuu/SS23/crypto/merkle"
+	"log"
+)
 
 type receiver struct {
+	n        int
+	t        int
 	id       int
 	acquired bool
 	voted    bool
@@ -12,8 +17,10 @@ type receiver struct {
 
 	root []byte
 
-	echos   int          // count echo msg
-	votes   int          // count vote msg
+	echos   []bool       // store nodes have sent echo
+	votes   []bool       // store nodes have sent vote
+	echoNum int          // count echo msg
+	voteNum int          // count vote msg
 	sendCh  chan Message // store send msg
 	echoCh  chan Message // store echo msg
 	voteCh  chan Message // store vote msg
@@ -42,7 +49,6 @@ func (receiver *receiver) handleMsgIn() {
 			//get a echo msg
 			//receiver.log.Println("get a echo msg from ", msg.DestID)
 			if !receiver.voted {
-
 				receiver.echoCh <- msg
 			}
 			break
@@ -61,14 +67,69 @@ func (receiver *receiver) handleMsgIn() {
 
 }
 
-func (receiver *receiver) handleSend() {
-	receiver.acquired = true
+type echobuf struct {
+	root       []byte
+	witness_i  merkle.Witness
+	root_i     []byte
+	witness_ij merkle.Witness
+	s_ij       []byte
+	f_ij       []byte
 }
 
 func (receiver *receiver) handleEcho() {
-	receiver.voted = true
+	var msg Message
+	for {
+		select {
+		case <-receiver.closeCh:
+			return
+
+		case msg = <-receiver.echoCh:
+		}
+		// get root from echo msg
+		id := msg.Type
+		if !receiver.echos[id] {
+			receiver.echos[id] = true
+			receiver.echoNum++
+		} else {
+			// receive duplicate message
+			continue
+		}
+		if receiver.echoNum == receiver.n-receiver.t && !receiver.voted {
+			// send vote msg
+			receiver.voted = true
+		}
+	}
+
 }
 
 func (receiver *receiver) handleVote() {
-	receiver.voted = false
+	var msg Message
+	for {
+		select {
+		case <-receiver.closeCh:
+			return
+
+		case msg = <-receiver.voteCh:
+		}
+		// get root from echo msg
+		id := msg.Type
+		if !receiver.votes[id] {
+			receiver.votes[id] = true
+			receiver.voteNum++
+		} else {
+			// receive duplicate message
+			continue
+		}
+		if receiver.voteNum == receiver.t+1 && !receiver.voted {
+			// send vote msg
+			votemsg := Message{
+				Type: VOTE,
+			}
+			receiver.voted = true
+		}
+		if receiver.voteNum == receiver.n-receiver.t && !receiver.done {
+			// out put stage
+
+		}
+	}
 }
